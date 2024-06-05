5ac8ecc12990821c32b2af2226c8bc3180d21c60
1429013747@qq.com <1429013747@qq.com>
2024-06-05 17:39:50 +0800

    add

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+console.log("first commit");
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: ["plugin:vue/essential", "eslint:recommended"],
+  parserOptions: {
+    parser: "@babel/eslint-parser",
+  },
+  rules: {
+    semi: [2, "always"], //不加分号报错 0:关闭 1:警告 2:错误
+    quotes: [2, "double"], //引号类型 `` "" ''
+    "prefer-const": 2, // 要求使用 const 声明那些声明后不再被修改的变量
+    camelcase: 2, //强制使用骆驼拼写法命名约定
+  },
+};
--- vue.config.js
+++ vue.config.js
@@ -52,4 +52,5 @@ module.exports = {
       pages: "@/pages",
     });
   },
+  lintOnSave: true, // 是否在开发环境下通过 eslint-loader 在每次保存时 lint 代码
 };
